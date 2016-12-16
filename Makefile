67735d17322a26558e7d1186520b7a3a1cbea15b
Igor Gajsin <igajsin@mirantis.com>
2016-12-16 18:39:09 +0300

    add support of the CLI options

---
+++ Makefile
@@ -0,0 +1,25 @@
+OCB_FLAGS = -use-ocamlfind -I src
+OCB = 		ocamlbuild $(OCB_FLAGS)
+
+all: 		native byte # profile debug
+
+clean:
+			$(OCB) -clean
+
+native:
+			$(OCB) bred.native
+
+byte:
+			$(OCB) bred.byte
+
+profile:
+			$(OCB) -tag profile bred.native
+
+debug:
+			$(OCB) -tag debug bred.byte
+
+#test: 		native
+#			./bred.native "OCaml" "OCamlBuild" "users"
+
+
+.PHONY: 	all clean byte native profile debug #test
---
+++ src/bred.ml
@@ -0,0 +1,45 @@
+(*main project*)
+
+open Cmdliner;;
+
+type output = Stdout | Outfile of string;;
+
+let string_of_output = function
+  | Stdout -> "stdout"
+  | Outfile file -> file;;
+
+let main deep out files num =
+  let fls = String.concat ", " files in
+  let outs = string_of_output out in
+  Printf.printf "deep = %i\nout = %s\nfiles = %s\nnum = %i\n" deep outs fls num;;
+
+let deep =
+  let doc = "How deep the chain?" in
+  Arg.(value & opt int 2 & info ["d"; "deep"] ~docv:"DEEP" ~doc);;
+
+let out =
+    let doc = "File to output the result." in
+    let outfile =
+      let parse s = `Ok (Outfile s) in
+      parse, fun ppf o -> Format.fprintf ppf "%s" (string_of_output o) in
+    Arg.(value & opt outfile Stdout & info ["o"; "output"] ~docv:"Outfile" ~doc);;
+
+let files =
+  Arg.(non_empty & pos_all file [] & info []  ~docv:"FILES");;
+
+let num =
+  (*Should be something like Infinite|Words of int| Lines of int*)
+  let doc = "Maximum length of resutl text." in
+  Arg.(value & opt int 0 & info ["n"; "num"] ~docv:"Amount of words" ~doc);;
+
+let main_t = Term.(const main $ deep $ out $ files $ num);;
+
+let info =
+  let doc = "Make a random text" in
+  let man = [`S "BUGS"; `P "email me";] in
+  Term.info "bred" ~version:"0.0.1" ~doc ~man
+
+
+let () = match Term.eval (main_t, info) with
+  | `Error _ -> exit 1
+  | _ -> exit 0;;
